b765edec3d07722aba9e4812cf27512dc2b2f056
Mehedi Hasan Antor <antorinfo@gmail.com>
2021-08-02 11:48:10 +0600

    update

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const quoteContainer = document.querySelector('#quote-container')
-const quoteText = document.querySelector('#quote')
-const authorText = document.querySelector('#author')
-const twitterBtn = document.querySelector('#twitter')
-const newQuoteBtn = document.querySelector('#new-quote')
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
 
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-  const apiUrl =
-    'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const apiUrl = 'https://goquotes-api.herokuapp.com/api/v1/random?count=10';
   try {
-    const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    authorText.innerText = data.quoteAuthor;
-    quoteText.innerText = data.quoteText;
-    console.log(data);
+    const response = await fetch(apiUrl);
+    const { quotes } = await response.json();
+
+    quotes.forEach((quote) => {
+      authorText.innerText = quote.author;
+      quoteText.innerText = quote.text;
+
+      if (quote.author === '') {
+        authorText.innerText = 'Unknown';
+      } else {
+        authorText.innerHTML = quote.author;
+      }
+      if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+      } else {
+        quoteText.classList.remove('long-quote');
+      }
+    });
   } catch (error) {
-    //   getQuote()
-      console.log(error);
+    console.log(error);
   }
 }
+
+newQuoteBtn.addEventListener('click', () => {
+  getQuote();
+});
 
 getQuote();
